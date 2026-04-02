6f8efb98c3a77807a4247db0a00155b5b22076a3
Sumit Pandey <sumit1p3@gmail.com>
2026-04-02 20:50:24 +0530

    Delete backend directory

--- backend/src/routes/syncRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { syncMessages } = require('../controllers/sosController');
-const { protect } = require('../middleware/auth');
-
-// Sync offline-queued messages from a device back to the server
-router.post('/messages', protect, syncMessages);
-
-module.exports = router;
